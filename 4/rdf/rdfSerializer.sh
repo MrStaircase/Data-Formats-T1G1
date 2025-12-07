e4326d62ae21bccd20af091195b331fedafb153e
khanhntp <kn977569@gmail.com>
2025-12-07 21:10:05 +0100

    Change in foaf: https to http

--- 4/rdf/rdfSerializer.sh
+++ 4/rdf/rdfSerializer.sh
@@ -7,7 +7,7 @@ rdf serialize \
 ex:https://example.org/vocabulary/,\
 emp:https://example.org/resource/employee/,\
 cus:https://example.org/resource/customer/,\
-foaf:https://xmlns.com/foaf/0.1/,\
+foaf:http://xmlns.com/foaf/0.1/,\
 shop:https://example.org/resource/shop/,\
 logo:https://example.org/resource/logo/,\
 prod:https://example.org/resource/product/,\
